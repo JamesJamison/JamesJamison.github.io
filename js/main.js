adfdb4a363af2788bcae9ed431a4658ac22973ac
jamison904 <james.jamison85@gmail.com>
2015-02-11 10:15:57 -0500

    Make clean

--- js/main.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  $(".mobile-header-menu").click(function() {
-    $(".mobile-link-list").slideToggle()
-
-    var arrow = $(".menu-arrow");
-    if (arrow.text() == "▼") arrow.text("▲");
-    else arrow.text("▼");
-  });
-})()
